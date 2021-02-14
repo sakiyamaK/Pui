fd8eddddcca74db62214f5bdbb7aaef45c9459e4
sakiyamaK <sakiyama.k@gmail.com>
2021-02-15 01:19:24 +0900

    add template

---
+++ Sources/PuiCore/Template/Template.swift
@@ -0,0 +1,23 @@
+//
+//  Template.swift
+//  ArgumentParser
+//
+//  Created by sakiyamaK on 2021/02/12.
+//
+
+import Foundation
+
+protocol Template {
+  var dirPath: String { get }
+  var fileName: String { get }
+  var code: String { get }
+  func save(file: FileOperator) throws
+}
+
+extension Template {
+  func save(file: FileOperator) throws {
+    try file.createDirectory(for: dirPath)
+    file.createFile(for: dirPath + fileName)
+    try file.write(to: dirPath + fileName, content: code)
+  }
+}
